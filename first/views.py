acbf1302e364c74ee5ae338d6cb256aa5eb3fa54
Millennial Academy <Ngonidzashe>
2020-05-16 19:00:09 +0200

    First version of application moved into github

---
+++ first/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
+from  mukumba.models import Users
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.mail import send_mail
+
+
+@login_required
+def home(request):
+    return render (request,"home.html", {})
